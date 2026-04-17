b4caeceb24ec971da116294d48ad6fbc33e5bf43
baomhtq2004 <phamngocbao0962@gmail.com>
2026-04-17 19:20:43 +0700

    update btl

---
+++ Gamesudoku/sudoku-api/storage/framework/views/5a8655ec3efeef76c5c42152ad268925.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\Admin\Documents\GitHub\gamesudoku\flutter_application_1\sudoku-api\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
